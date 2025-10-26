73f51ba205a8a4e41a2ec5519b96c59c1f5dd0ee
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-26 10:43:47 +0700

    remove hero section customization and load customizer modules

--- include/customizer.php
+++ include/customizer.php
@@ -1,46 +1,5 @@
-<?php 
+<?php
 
-// register customizer theme
-function customize_register($wp_customize)
-{
-    $wp_customize->add_section('hero_section', array(
-        'title'      => __('Hero Section', 'abuhasdha'),
-        'priority'   => 30,
-    ));
-
-    $wp_customize->add_setting('hero_image');
-    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_image_control', array(
-        'label'      => __('Hero Image', 'abuhasdha'),
-        'section'    => 'hero_section',
-        'settings'   => 'hero_image',
-    )));
-
-    $wp_customize->add_setting('hero_heading', array('default' => 'Solusi Ban Alat Berat dan Industri Anda'));
-    $wp_customize->add_control('hero_heading_control', array(
-        'label'      => __('Judul Hero', 'abuhasdha'),
-        'section'    => 'hero_section',
-        'settings'   => 'hero_heading',
-        'type'       => 'text',
-    ));
-
-    $wp_customize->add_setting('hero_description', array('default' => 'Dengan pengalaman lebih dari 40 tahun, Abuhasdha menyediakan ban Off-The-Road (OTR), truk, bus, dan industri yang tangguh, berkualitas, dan didukung layanan teknis menyeluruh.'));
-    $wp_customize->add_control('hero_description_control', array(
-        'label'      => __('Deskripsi Hero', 'abuhasdha'),
-        'section'    => 'hero_section',
-        'settings'   => 'hero_description',
-        'type'       => 'textarea',
-    ));
-
-    $wp_customize->add_setting('hero_button_text', array('default' => 'Lihat Produk'));
-    $wp_customize->add_control('hero_button_text_control', array(
-        'label'      => __('Teks Tombol', 'abuhasdha'),
-        'section'    => 'hero_section',
-        'settings'   => 'hero_button_text',
-        'type'       => 'text',
-    ));
-
-    // remove unnecessary section
-    $wp_customize->remove_section('static_front_page');
-    $wp_customize->remove_section('custom_css');
+foreach (glob(get_template_directory() . '/include/customizer/*.php') as $file) {
+    require_once $file;
 }
-add_action('customize_register', 'customize_register');
