710bf5aac34abaa768c20b48047a9fac32b5d5d5
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-28 12:03:10 +0700

    remove product-slug-maker include file as it's no longer needed

--- functions.php
+++ functions.php
@@ -12,5 +12,4 @@ require get_template_directory() . '/include/customizer.php';
 require get_template_directory() . '/include/dashboard.php';
 require get_template_directory() . '/include/custom-login.php';
 require get_template_directory() . '/include/nav.php';
-require get_template_directory() . '/include/product-slug-maker.php';
 require get_template_directory() . '/include/override-acf.php';
