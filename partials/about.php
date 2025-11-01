c046a0434d5a31937bcc28ffc2576972ad006a87
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-01 22:08:20 +0700

    add about section with image and description from theme customizer

---
+++ partials/about.php
@@ -0,0 +1,21 @@
+<?php
+
+$image_url = esc_url(get_theme_mod('about_image', get_template_directory_uri() . '/assets/about.png'));
+$description = nl2br(get_theme_mod('about_description'));
+
+?>
+
+<section id="about" class="w-full">
+    <div class="md:h-[80vh] h-auto py-18 flex flex-col gap-6 justify-center items-center px-8 md:px-24">
+        <div class="text-left w-full relative flex overflow-hidden">
+            <h2 class="text-3xl font-bold text-dark-orange after:ml-2 md:after:w-full after:h-[3px] after:bg-dark-orange after:absolute after:bottom-1">Tentang Abuhasdha</h2>
+        </div>
+
+        <div class="flex md:flex-row flex-col gap-6 items-center justify-center">
+            <img class="w-[300px]" src="<?= $image_url ?>" alt="about">
+            <div class="flex flex-col gap-6">
+                <?= $description ?>
+            </div>
+        </div>
+    </div>
+</section>
