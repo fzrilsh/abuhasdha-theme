bc724af657149e604c98bd14bebe92090e86ba35
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-02 20:33:52 +0700

    remove glob usage and include customizer files explicitly

--- include/customizer.php
+++ include/customizer.php
@@ -1,5 +1,7 @@
 <?php
 
-foreach (glob(get_template_directory() . '/include/customizer/*.php') as $file) {
-    require_once $file;
-}
+require_once get_template_directory() . '/include/customizer/hero-customizer.php';
+require_once get_template_directory() . '/include/customizer/about-customizer.php';
+require_once get_template_directory() . '/include/customizer/technical-customizer.php';
+require_once get_template_directory() . '/include/customizer/gallery-customizer.php';
+require_once get_template_directory() . '/include/customizer/footer-customizer.php';
