1c0b44ce449b15fcd44a47424809d74a25962f6e
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-03 22:00:04 +0700

    add cache functionality with include file

--- functions.php
+++ functions.php
@@ -13,3 +13,4 @@ require get_template_directory() . '/include/dashboard.php';
 require get_template_directory() . '/include/custom-login.php';
 require get_template_directory() . '/include/custom-nav.php';
 require get_template_directory() . '/include/override-acf.php';
+require get_template_directory() . '/include/cache.php';
