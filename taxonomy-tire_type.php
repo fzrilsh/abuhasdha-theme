9d839f19fed4a494911e5727cd615c16207c3139
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-03 13:41:12 +0700

    add tire_type taxonomy template with size filtering and redirect if no products found

---
+++ taxonomy-tire_type.php
@@ -0,0 +1,36 @@
+<?php
+
+$size = get_query_var('size');
+$type = get_query_var('tire_type');
+$products = new WP_Query([
+    'post_type' => 'product',
+    'posts_per_page' => -1,
+    'tax_query' => [
+        [
+            'taxonomy' => 'tire_type',
+            'field'    => 'slug',
+            'terms'    => $type,
+        ],
+    ],
+    'meta_query' => [
+        [
+            'key'     => 'size',
+            'value'   => $size,
+            'compare' => '=',
+        ]
+    ],
+]);
+
+if(!$products->have_posts()) {
+    wp_safe_redirect(home_url('/products'));
+    exit;
+}
+
+get_header(); 
+
+set_query_var('products', $products);
+require_once get_template_directory() . '/partials/products.php';
+
+get_footer();
+
+?>
