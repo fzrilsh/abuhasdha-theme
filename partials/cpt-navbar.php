22f6e89939c94f9c7d743d7072978910b553191b
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-03 13:40:58 +0700

    add cpt navbar with back to products or home link

---
+++ partials/cpt-navbar.php
@@ -0,0 +1,12 @@
+<nav class="w-full flex justify-between items-center fixed top-0 z-50">
+    <div class="w-full flex justify-between items-center relative bg-white">
+        <a href="<?= esc_url(home_url('/')); ?>" class="md:px-8 px-4 py-4">
+            <img src="<?= get_theme_mod('custom_logo') ? wp_get_attachment_url(get_theme_mod('custom_logo'), 'full') : get_stylesheet_directory_uri() . '/assets/logo.png' ?>" alt="Logo">
+        </a>
+
+        <a href="<?= esc_url(home_url(get_query_var('size') ? '/products' : '/')) ?>" class="h-full px-8 md:px-12 font-bold text-orange flex flex-row items-center justify-center gap-3 text-lg">
+            <i class="fa-solid fa-chevron-left"></i>
+            <p class="hidden md:flex"><?= get_query_var('size') ? 'Daftar Produk' : 'Kembali ke Beranda' ?></p>
+        </a>
+    </div>
+</nav>
