a244c6a117ffec9edb78eacc22f7d22155de7af2
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-03 08:47:14 +0700

    add shadow to navbar for better visibility

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,4 +1,4 @@
- <nav class="w-full flex justify-between items-center fixed top-0 z-50">
+ <nav class="w-full flex justify-between items-center fixed top-0 z-50 shadow-lg">
     <div class="w-full flex justify-between relative bg-white">
         <a href="<?= esc_url(home_url('/')); ?>" class="md:px-8 px-4 py-2">
             <img src="<?= get_theme_mod('custom_logo') ? wp_get_attachment_url(get_theme_mod('custom_logo'), 'full') : get_stylesheet_directory_uri() . '/assets/logo.png' ?>" alt="Logo">
@@ -22,7 +22,7 @@
     wp_nav_menu(array(
         'theme_location' => 'primary_menu',
         'container' => 'ul',
-        'menu_class' => 'hidden md:hidden absolute top-[100%] bg-white w-full text-white',
+        'menu_class' => 'hidden md:hidden absolute top-[100%] bg-white w-full text-white shadow-lg',
         'menu_id' => 'mobilebar'
     ));
     ?>
